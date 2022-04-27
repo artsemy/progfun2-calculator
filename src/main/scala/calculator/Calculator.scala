c1f1b9ec7ac2c5ab3fbe492f60807b0e5f2b1eb6
artsemy <artemiy.konoplyanik@gmail.com>
2022-04-27 09:18:07 +0300

    attempt 1

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -8,21 +8,35 @@ enum Expr:
   case Times(a: Expr, b: Expr)
   case Divide(a: Expr, b: Expr)
 
-object Calculator extends CalculatorInterface:
- import Expr.*
+object Calculator extends CalculatorInterface :
+
+  import Expr.*
 
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
-    ???
+    namedExpressions.map {
+      case (key, sig) => (key, Signal(eval(sig(), namedExpressions)))
+    }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]])(using Signal.Caller): Double =
-    ???
+    expr match {
+      case Expr.Literal(v) => v
+      case Expr.Ref(name) =>
+        if references.contains(name) then
+          eval(references(name)(), references.removed(name))
+        else
+          Double.NaN
+      case Expr.Plus(a, b) => eval(a, references) + eval(b, references)
+      case Expr.Minus(a, b) => eval(a, references) - eval(b, references)
+      case Expr.Times(a, b) => eval(a, references) * eval(b, references)
+      case Expr.Divide(a, b) => eval(a, references) / eval(b, references)
+    }
 
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
+   * If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]])(using Signal.Caller): Expr =
+                               references: Map[String, Signal[Expr]])(using Signal.Caller): Expr =
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,8 +3,16 @@ package calculator
 object Polynomial extends PolynomialInterface:
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] =
-    ???
+    Signal (b() * b() - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+    Signal {
+      val d = delta()
+      if d < 0 then
+        Set()
+      else if d == 0 then
+        Set(-b() / 2 / a())
+      else
+        Set((-b() + Math.sqrt(d)) / 2 / a(), (-b() - Math.sqrt(d)) / 2 / a())
+    }
